45f69aaf8254048406b3398b4b766d782c074017
Udal Khatik <udal.khatik@aidash.com>
2025-07-10 21:47:36 +0530

    added bug fix

--- src/mode.js
+++ src/mode.js
@@ -109,11 +109,9 @@ SplitPolygonMode.drawAndSplit = function (state) {
           }
         });
         
-        console.log({ polygons: newPolygons });
-        
         this.fireUpdate(newPolygons);
         this.highlighFeatures(state, false);
-        if (newPolygons.length !== 1 || (newPolygons[0].geometry.type === 'MultiPolygon' && newPolygons[0].geometry.coordinates.length !== 2) || isLineWithinPolygon) {
+        if (isLineWithinPolygon || newPolygons.length !== 1 || (newPolygons[0].geometry?.type === 'MultiPolygon' && newPolygons[0].geometry?.coordinates?.length !== 2)) {
           this.map.fire(splitPolygonModeEvents.SPLIT_POLYGON_MORE_THAN_TWO_POLYGONS, {
             newPolygons,
           });
